f4d917130a81488f512ddbca560207b1d2b6af67
felcillive <belofelcita@gmail.com>
2019-08-25 00:01:56 +0300

    feat: add webpack, babel,eslint

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { enforce: 'pre', test: /\.js$/, loader: 'eslint-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader'],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'My Awesome application',
+      myPageHeader: 'Hello World',
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new ExtractTextPlugin({ filename: 'style.css' }),
+  ],
+};
